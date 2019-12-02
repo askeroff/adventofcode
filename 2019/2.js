822b8cdef4a75e7eb94f00bfb2fde525d61a065c
Javid Askerov <javid.as@goldenpay.az>
2019-12-02 13:43:13 +0400

    day 2 solution in js

---
+++ 2019/2.js
@@ -0,0 +1,51 @@
+(function() {
+    // let data = document.getElementsByTagName('pre')[0].innerHTML.split(',');
+    // let intData = data.map(item => +item);
+    // intData[1] = 12;
+    // intData[2] = 2;
+
+    function part1(intData) {   
+        const operations = {
+            1: sum,
+            2: multiply
+        };
+
+        function sum(a, b) {
+            return intData[a] + intData[b];
+        }
+
+        function multiply(a,b) {
+            return intData[a] * intData[b];
+        }
+        
+        for (let i = 0; i < intData.length; i += 4) {
+            if (intData[i] !== 99 && operations[intData[i]]) {
+                const operation = operations[intData[i]];
+                const pos = intData[i+3] ;
+                intData[pos] =  operation(intData[i+1], intData[i+2]);
+            }
+        }
+        // console.log(intData[0]);
+        return intData[0];
+    }
+
+    // part1(intData);
+
+    function part2() {
+        let answer = [];
+        for(let i = 0; i <= 99; i++) {
+            for(let z = 0; z <= 99; z++) {
+                let myData = document.getElementsByTagName('pre')[0].innerHTML.split(',').map(item => +item);
+                myData[1] = i;
+                myData[2] = z;
+                const result = part1(myData);
+                if(result === 19690720) {
+                    console.log(i, z, 'ANSWER');
+                }
+            }
+        }
+    }   
+
+    part2();
+
+})();
