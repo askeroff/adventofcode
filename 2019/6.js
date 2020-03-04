5767d79b1a7a754d81d830652b69c715db9f819c
Javid Askerov <javid.as@goldenpay.az>
2020-03-04 13:45:15 +0400

    add indirect orbits and solve part 2

--- 2019/6.js
+++ 2019/6.js
@@ -1,35 +1,54 @@
 (function () {
   let data = document.getElementsByTagName('pre')[0].innerHTML.split('\n');
   data.pop();
-  console.log(data);
 
   const orbits = {};
 
   data.forEach(item => {
     const items = item.split(")");
     if (orbits[items[1]] === undefined) {
-      orbits[items[1]] = items[0];
+      orbits[items[1]] = {direct: items[0]};
     }
   });
 
-  function getOrbit(prop) {
+  function countOrbits(prop, obj = orbits) {
     let i = 0;
     let p = prop;
-    while (orbits[p]) {
-      p = orbits[p];
+    while (obj[p]) {
+      p = obj[p].direct;
+      if (obj[prop]) {
+        obj[prop].indirect = obj[prop].indirect ? obj[prop].indirect : [];
+        obj[prop].indirect.push(p);
+      }
       i += 1;
     }
     return i;
   }
 
-  let sum = 0;
-
-  for (let key in orbits) {
-    if (orbits[key]) {
-      const test = getOrbit(key);
-      sum += test;
+  function part1() {
+    let sum = 0;
+    for (let key in orbits) {
+      if (orbits[key]) {
+        const test = countOrbits(key);
+        sum += test;
+      }
     }
+    console.log(sum, 'answer to part 1');
   }
 
-  console.log(sum, orbits);
+  part1();
+
+  function part2() {
+    let min = 0;
+    orbits['YOU'].indirect.forEach((item, index) => {
+      orbits['SAN'].indirect.forEach((secondItem, secondIndex) => {
+        if (item === secondItem && min === 0) {
+          min = index + secondIndex
+        }
+      })
+    });
+    console.log(min, 'answer to second part');
+  }
+
+  part2();
 })();
